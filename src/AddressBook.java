464edfe7d7eb7ab477630bcfb373f72c6a3d0044
Khalid <khalidkanaan@cmail.carleton.ca>
2020-09-25 10:59:21 -0400

    new commit file

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -2,16 +2,23 @@ import java.util.ArrayList;
 
 public class AddressBook {
     private ArrayList<BuddyInfo> Collection;
-    public AddressBook(){
+
+    public AddressBook() {
         Collection = new ArrayList<>();
     }
-    void addBuddy(BuddyInfo buddy){
+
+    void addBuddy(BuddyInfo buddy) {
         Collection.add(buddy);
     }
+
     void removeBuddy(BuddyInfo buddy) {
         Collection.remove(buddy);
     }
-    public static void main(String[] args){
-        System.out.println("Address book");
+
+    public static void main(String[] args) {
+        BuddyInfo buddy = new BuddyInfo("Tom", "carleton", 613);
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
     }
 }
